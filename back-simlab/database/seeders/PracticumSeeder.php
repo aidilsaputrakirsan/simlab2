cc3fc54a7a1710ee972b7b5a896f2a3fead37be9
Myst-ADL <89290578+aidilsaputrakirsan@users.noreply.github.com>
2026-01-12 14:39:02 +0800

    update seeder

--- back-simlab/database/seeders/PracticumSeeder.php
+++ back-simlab/database/seeders/PracticumSeeder.php
@@ -3,38 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\Practicum;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\StudyProgram;
 use Illuminate\Database\Seeder;
 
 class PracticumSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
+        $prodiId = StudyProgram::first()->id ?? 1;
+
         Practicum::insert([
-            [
-                'code' => 'IS23123',
-                'name' => 'Pemrogramman Web',
-                'study_program_id' => 10,
-                'type' => 'compulsory',
-                'sks' => 3
-            ],
-            [
-                'code' => 'IS31231',
-                'name' => 'Pemrogramman Beroriantasi Objek',
-                'study_program_id' => 10,
-                'type' => 'compulsory',
-                'sks' => 3
-            ],
-            [
-                'code' => 'IS33123',
-                'name' => 'Cloud Computing',
-                'study_program_id' => 10,
-                'type' => 'compulsory',
-                'sks' => 2
-            ],
+            ["code" => "IS23123", "name" => "Pemrogramman Web", "study_program_id" => $prodiId, "type" => "compulsory", "sks" => 3],
+            ["code" => "IS31231", "name" => "Pemrogramman Beroriantasi Objek", "study_program_id" => $prodiId, "type" => "compulsory", "sks" => 3],
+            ["code" => "IS33123", "name" => "Cloud Computing", "study_program_id" => $prodiId, "type" => "compulsory", "sks" => 2],
         ]);
     }
 }
--- back-simlab/database/seeders/UserSeeder.php
+++ back-simlab/database/seeders/UserSeeder.php
@@ -3,40 +3,45 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         User::insert([
             [
-                'name' => 'Admin (Erlina)',
-                'email' => 'labterpadu@itk.ac.id',
-                'password' => bcrypt('123123'),
-                'role' => 'admin',
-                'identity_num' => null,
-                'study_program_id' => null,
+                "name" => "Admin (Erlina)",
+                "email" => "labterpadu@itk.ac.id",
+                "password" => bcrypt("123123"),
+                "role" => "admin",
+                "identity_num" => null,
+                "study_program_id" => null,
+                "institution_id" => null,
+                "is_manager" => false,
+                "is_active" => "Active",
             ],
             [
-                'name' => 'Kepalalab',
-                'email' => 'kepalalab@gmail.com',
-                'password' => bcrypt('123123'),
-                'role' => 'kepala_lab_terpadu',
-                'identity_num' => '2313321',
-                'study_program_id' => 10,
+                "name" => "Kepalalab",
+                "email" => "kepalalab@gmail.com",
+                "password" => bcrypt("123123"),
+                "role" => "kepala_lab_terpadu",
+                "identity_num" => "2313321",
+                "study_program_id" => null,
+                "institution_id" => null,
+                "is_manager" => false,
+                "is_active" => "Active",
             ],
             [
-                'name' => 'laboran',
-                'email' => 'laboran@gmail.com',
-                'password' => bcrypt('123123'),
-                'role' => 'laboran',
-                'identity_num' => null,
-                'study_program_id' => null,
+                "name" => "laboran",
+                "email" => "laboran@gmail.com",
+                "password" => bcrypt("123123"),
+                "role" => "laboran",
+                "identity_num" => null,
+                "study_program_id" => null,
+                "institution_id" => null,
+                "is_manager" => false,
+                "is_active" => "Active",
             ]
         ]);
     }
